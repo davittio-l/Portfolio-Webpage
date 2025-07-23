633bb71c1733bda25f8737d6f7a47008356da1a1
Colten <129473270+Davittio-L@users.noreply.github.com>
2025-07-23 15:09:52 -0400

    converted views to 'cards' and pdf reader.

---
+++ project2.js
@@ -0,0 +1,11 @@
+
+  function openPdf(pdfUrl) {
+    document.getElementById("pdf-frame").src = pdfUrl;
+    document.getElementById("pdf-modal").style.display = "block";
+  }
+
+  function closePdf() {
+    document.getElementById("pdf-frame").src = "";
+    document.getElementById("pdf-modal").style.display = "none";
+  }
+
