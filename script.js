119d5d66839f0a5b96f41fbbec27635cfba1e931
Colten <129473270+Davittio-L@users.noreply.github.com>
2025-08-07 13:07:41 -0400

    added API Gateway

---
+++ script.js
@@ -0,0 +1,15 @@
+fetch("https://lms2rcmkpk.execute-api.us-east-2.amazonaws.com/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Colten Bayles",
+      email: "colten@example.com",
+      message: "Testing the contact form!",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("Success:", data))
+    .catch((err) => console.error("Error:", err));
+  
