e8dd36e72b30f48a64b2417d5b57d713f2be4d9b
Colten <129473270+Davittio-L@users.noreply.github.com>
2025-08-08 11:01:38 -0400

    edited script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-fetch("https://lms2rcmkpk.execute-api.us-east-2.amazonaws.com/live", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: "Colten Bayles",
-      email: "colten@example.com",
-      message: "Testing the contact form!",
-    }),
+fetch("https://lms2rcmkpk.execute-api.us-east-2.amazonaws.com/live/contact", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    name: "Colten Bayles",
+    email: "colten@example.com",
+    message: "Testing the contact form!"
   })
-    .then((res) => res.json())
-    .then((data) => console.log("Success:", data))
-    .catch((err) => console.error("Error:", err));
-  
+})
+.then(res => res.json())
+.then(data => console.log("Success:", data))
+.catch(err => console.error("Error:", err));
